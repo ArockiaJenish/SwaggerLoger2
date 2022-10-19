7a45be9290f983cbbba23409a9ad7ca0ee18826e
[Arockia Jenish] <[arockiajenish97@gmail.com]>
2022-10-19 12:03:00 +0530

    main

--- src/main/java/com/jenish/demo/exceptions/UserException.java
+++ src/main/java/com/jenish/demo/exceptions/UserException.java
@@ -2,6 +2,10 @@ package com.jenish.demo.exceptions;
 
 public class UserException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 	private String message;
 	
